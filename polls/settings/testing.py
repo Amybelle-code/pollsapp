71b90f4c6c47b5a07f88853cdb8109d49102539f
Amarachi <aechendujoe@gmail.com>
2021-03-07 02:22:04 +0100

    Edit testing.py

--- polls/settings/testing.py
+++ polls/settings/testing.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'github_actions',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
